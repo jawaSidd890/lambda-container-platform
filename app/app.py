7773803dc18d37e6027e1d546a4a8aa792d65298
Jawahar D <jed664700@gmail.com>
2025-12-29 14:08:58 +0530

    Update app.py

---
+++ app/app.py
@@ -0,0 +1,6 @@
+def handler(event, context):
+    name = event.get("queryStringParameters", {}).get("name", "World")
+    return {
+        "statusCode": 200,
+        "body": f"Hello {name}"
+    }
